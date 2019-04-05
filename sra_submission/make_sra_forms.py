e91c31f14e2d43307c3555f7d84dee1db258355b
Danny Lawrence <djpl@uw.edu>
2019-04-05 10:19:06 -0700

    cleaned up make_sra_forms.py

--- sra_submission/make_sra_forms.py
+++ sra_submission/make_sra_forms.py
@@ -6,63 +6,58 @@ import sys
 import pandas as pd
 import tarfile
 
-# The experiments are listed in `experiments.csv` There are six different concentrations, three biological replicates, and various technical replicates of S139/1. There are two concentrations, three biological replicates, and various technical replicates of C179. There are also four mock-selected samples (two for library 1, and one each for libraries 2 and 3), and finally a WT plasmid control.
-exptsdf = pd.read_csv('./experiment_list.csv', nrows=29)
-samples = exptsdf['experiment'].tolist()
+# The experiments are listed in `samplelist.csv` 
+exptsdf = (pd.read_csv('../data/samplelist.csv')
+			.assign(sample_name=lambda x: x.selection + '_' + x.library))
 
-# where are the FASTQ files?
-origpath_dict = {'A': '/fh/fast/bloom_j/SR/ngs/illumina/bloom_lab/170609_SN367_0933_BHMLH5BCXY_lane2/Unaligned/Project_bloom_lab/',
-                 'B': '/fh/fast/bloom_j/SR/ngs/illumina/bloom_lab/180427_SN367_1155_BHHJMKBCX2/Unaligned/Project_bloom_lab/',
-                 'C': '/fh/fast/bloom_j/SR/ngs/illumina/bloom_lab/180928_D00300_0621_BHMJGNBCX2/Unaligned/Project_bloom_lab/'}
-# the files themselves are in subdirectories of this directory, each sample name is a subdirectory.
 
-# new biosample added (Sept 2016) to the existing bioproject of WSN HA libraries; this sample is labeled something like
-# "WSN HA libraries selected with monoclonal antibodies":
-biosampleid = 'SAMN05789126' # this biosampleid was created for the initial mab datasets and should be OK to use for the additional FI6v3 data.
-# the bioproject number is PRJNA309339 for WSN HA sequencing.
-bioprojectid = 'PRJNA309339' # this bioprojectid was created for the viruses paper when first describing the virus libraries; should be OK to continue to use this.
+biosampleid = 'SAMN11335355' # this biosampleid was created for the initial ZIKV DMS and mab selection.
+# the bioproject number is PRJNA530795 for ZIKV DMS sequencing.
+bioprojectid = 'PRJNA530795' 
 
 tar_filename = 'ZIKV_E_Ab_selection.tar'
-make_tar = False # set to true to actually make the tarfile; probably want to test with *make_tar = False* at first to make sure all the files are found properly.
+make_tar = True # set to true to actually make the tarfile; probably want to test with *make_tar = False* at first to make sure all the files are found properly.
 
 fnew = open('submissionform2019_ZKA64-ZKA185.tsv','w')
 
 # header:
-fnew.write('bioproject_accession\tbiosample_accession\tlibrary_ID\ttitle\tlibrary_strategy\tlibrary_source\tlibrary_selection\tlibrary_layout\tplatform\tinstrument_model\tdesign_description\tfiletype\tfilename\tfilename2\tfilename3\tfilename4\tfilename5\tfilename6\n')
+fnew.write('bioproject_accession\tbiosample_accession\tlibrary_ID\ttitle\tlibrary_strategy\tlibrary_source\tlibrary_selection\tlibrary_layout\tplatform\tinstrument_model\tdesign_description\tfiletype\tfilename\tfilename2\tfilename3\tfilename4\tfilename5\tfilename6\tfilename7\tfilename8\tfilename9\tfilename10\tfilename11\tfilename12\tfilename13\tfilename14\tfilename15\tfilename16\tfilename17\tfilename18\tfilename19\tfilename20\tfilename21\tfilename22\tfilename23\tfilename24\n')
 
 # open a tar file to add all the fastq files to:
 if make_tar:
     tar_out = tarfile.open(tar_filename, mode='w')
 
 # go through samples, adding information to the submission form and adding FASTQ.gz files to the tar archive:
-for sample in samples:
-    print ('\nProcessing sample {0}...'.format(sample))
+for sample_tup in exptsdf.itertuples():
+    sample_name = sample_tup.sample_name
+    print ('\nProcessing sample {0}...'.format(sample_name))
 
-    FASTQ_directory = exptsdf[exptsdf['experiment'] == sample]['fastqdir'].item()
-    #FASTQ_directory = origpath_dict[FASTQ_key]
+    FASTQ_directory = sample_tup.R1
+    print(FASTQ_directory)
 
     # sample_name is "library_ID" on spreadsheet
-    sample_name = exptsdf[exptsdf['experiment'] == sample]['sample_name'].item()
+    selection = sample_tup.selection
+    library = sample_tup.library
 
     if ('ZKA64' in sample_name):
-        title = 'MR766 E library {0} neutralized with ZKA64'.format(sample_name[-1])
+        title = 'MR766 E library {0} neutralized with ZKA64'.format(library)
     elif ('ZKA185' in sample_name):
-        title = 'MR766 E library {0} neutralized with ZKA185'.format(sample_name[-1])
-    elif ('Mock' in sample_name):
-        title = 'MR766 E library {0} mock-neutralized'.format(sample_name[-1])
-    elif ('HCV' in sample_name):
-        title = 'MR766 E library {0} selected with control antibody AR3A'.format(sample_name[-1])
-    elif ('zikv-' in sample_name):
+        title = 'MR766 E library {0} neutralized with ZKA185'.format(library)
+    elif ('no-antibody' in sample_name):
+        title = 'MR766 E library {0} mock-neutralized'.format(library)
+    elif ('control-antibody' in sample_name):
+        title = 'MR766 E library {0} selected with control antibody AR3A'.format(library)
+    elif ('plasmid' in sample_name):
         title = 'MR766 E library {0} plasmid'.format(sample_name[5:])
-    elif ('zikv_' in sample_name):
-        title = 'MR766 E library {0} selected in Vero cell culture'.format(sample_name[5:])
+    elif ('virus' in sample_name):
+        title = 'MR766 E library {0} selected in Vero cell culture'.format(library)
 
 
     # Each sample is a row in the spreadsheet with several filenames at the end of the row, here is everything until the filenames:
     fnew.write('{0}\t{1}\t{2}\t{3}\tAMPLICON\tVIRAL RNA\tPCR\tpaired\tILLUMINA\tIllumina HiSeq 2500\t250-nt paired-end reads of Zika virus E PCR amplicons\tfastq\t'.format(bioprojectid,biosampleid,sample_name,title))
 
     # Now add a tab-spaced entry in this library_ID's row for each file that will be uploaded:
-    files = sorted(glob.glob('{0}Sample_{1}*/*.fastq.gz'.format(FASTQ_directory, sample_name)))
+    files = sorted(glob.glob(FASTQ_directory)+glob.glob(FASTQ_directory.replace('R1', 'R2')))
     print ('Found these files:'), files
     
     for fname in files:
